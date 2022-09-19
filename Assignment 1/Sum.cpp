6f8d8ffd64c8d033f58cb2be6dcf5c9ef0b25d8e
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-19 12:38:50 +0530

    Create Sum.cpp

---
+++ Assignment 1/Sum.cpp
@@ -0,0 +1,20 @@
+
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a,b,c;
+    //cout<<"enter 3 number";  
+    cin>>a>>b>>c;
+    int sum = a+b+c ;
+    cout<<sum;
+    return 0;
+}
