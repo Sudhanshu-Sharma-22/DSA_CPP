82853cbcf99fda05fe4ef2467a42c8828320e26a
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-19 16:30:15 +0530

    Create Conditional statements.c

---
+++ Assignment 2/Conditional statements.c
@@ -0,0 +1,49 @@
+
+//https://www.hackerrank.com/challenges/conditional-statements-in-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d", &a);
+      if (a==1) {
+          printf("one\n");
+          
+      }
+      else if (a==2) {
+          printf("two\n");          
+      }
+      else if (a==3) {
+          printf("three\n");          
+      }
+      else if (a==4) {
+          printf("four\n");          
+      }
+      else if (a==5) {
+          printf("five\n");          
+      }
+      else if (a==6) {
+          printf("six\n");          
+      }
+      else if (a==9) {
+          printf("nine\n");          
+      }
+      else if (a==7) {
+          printf("seven\n");          
+      }
+      else if (a==8) {
+          printf("eight\n");          
+      }
+      else {
+          printf("Greater than 9\n");
+      }
+      
+
+    return 0;
+}
