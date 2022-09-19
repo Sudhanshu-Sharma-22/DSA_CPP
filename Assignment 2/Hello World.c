b73ab421ea1b28490201176bd8f4614412d02707
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-19 13:13:13 +0530

    Create Hello World.c

---
+++ Assignment 2/Hello World.c
@@ -0,0 +1,17 @@
+
+//https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+	printf("Hello, World!\n");
+    char s[100];
+    fgets(s, 100, stdin);
+    printf("%s",s);
+    
+    
+    return 0;
+}
