11a5bc0d7abbfda0b990c58c753ca3ca387ce354
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-19 12:40:33 +0530

    Create Data Types.cpp

---
+++ Assignment 1/Data Types.cpp
@@ -0,0 +1,22 @@
+
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c<<"\n";
+    cout<<d<<"\n";
+    cout<<e<<"\n";
+    return 0;
+}
