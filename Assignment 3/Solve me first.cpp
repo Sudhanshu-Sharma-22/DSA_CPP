15650debcfb48a8e534ae2fcaea2ce012e037d32
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-27 21:10:26 +0530

    Create Solve me first.cpp

---
+++ Assignment 3/Solve me first.cpp
@@ -0,0 +1,19 @@
+
+//https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    return a+b;
+  
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
