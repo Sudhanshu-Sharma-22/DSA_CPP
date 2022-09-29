89451c60d75499839dd86e1758a027a1f27d10fb
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-29 23:03:08 +0530

    Update Time Conversion.cpp

--- Assignment 3/Time Conversion.cpp
+++ Assignment 3/Time Conversion.cpp
@@ -1,1 +1,20 @@
 
+//https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    //cout<<s;
+    int temp;
+    temp=s[0]+s[1];
+    if(temp=103){
+        s[0]=1;
+        s[1]=9;
+    }
+    
+    cout<<s;
+    return 0;
+}
