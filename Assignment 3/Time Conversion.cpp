e7bdf08f096d8a2f682fb6d02ca0d8af4ff29673
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-09-30 20:11:38 +0530

    Update Time Conversion.cpp

--- Assignment 3/Time Conversion.cpp
+++ Assignment 3/Time Conversion.cpp
@@ -4,17 +4,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string s;
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s,hr,min,sec;
     cin>>s;
-    //cout<<s;
-    int temp;
-    temp=s[0]+s[1];
-    if(temp=103){
-        s[0]=1;
-        s[1]=9;
+    hr[0]=s[0];
+    hr[1]=s[1];
+    min[0]=s[3];
+    min[1]=s[4];
+    sec[0]=s[6];
+    sec[1]=s[7];
+    if(s[8]==80){
+        int num=stoi(hr);
+        hr=to_string(num+12);
     }
-    
-    cout<<s;
+    else{
+        int num=stoi(hr);
+        hr=to_string(num+12);
+    }
+    if(hr[0]==50 && hr[1]==52){
+            hr[0]=48;
+            hr[1]=48;
+        }
+    cout<<hr[0]<<hr[1]<<":"<<min[0]<<min[1]<<":"<<sec[0]<<sec[1];
     return 0;
 }
