8ed680dd27d36a152720281cda8613a5a0159b28
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-10-22 10:22:09 +0530

    Update Number Line Jumps.cpp

--- Assignment 4/Number Line Jumps.cpp
+++ Assignment 4/Number Line Jumps.cpp
@@ -1,13 +1,17 @@
 
 //https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
 
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    int x1,v1,x2,v2;
+int main()
+{
+    int x1, v1, x2, v2;
     cin>>x1>>v1>>x2>>v2;
-    if(x1>x2 || v1>v2) cout<<"YES";
-    else cout<<"NO";
-    return 0;
+    if (v1>v2&&(x2-x1)%(v1-v2)==0)
+    {
+        cout<<"YES";
+    }
+    else {
+    cout<<"NO";
+    }
 }
