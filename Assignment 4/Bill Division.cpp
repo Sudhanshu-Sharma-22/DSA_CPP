162f170edda3a14ae4e965d38c064b5a72aa68b7
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-10-22 10:13:13 +0530

    Update Bill Division.cpp

--- Assignment 4/Bill Division.cpp
+++ Assignment 4/Bill Division.cpp
@@ -1,1 +1,21 @@
-//in progress
+
+//https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,k,sum=0;
+    cin>>n>>k;
+    vector<int>bill(n);
+    for(int i=0;i<n;i++){
+        cin>>bill[i];
+        if(i!=k) sum+=bill[i];
+    }
+    int charged;
+    cin>>charged;
+    sum=sum/2;
+    if(sum<charged) cout<<charged-sum;
+    else cout<<"Bon Appetit";
+    return 0;
+}
