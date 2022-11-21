a80999abdb35bda266599317f1a6079d803f6de2
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-11-21 12:07:09 +0530

    Update Stack using array.cpp

--- Assignment 6/Stack using array.cpp
+++ Assignment 6/Stack using array.cpp
@@ -1,1 +1,42 @@
-//in progress
+
+//https://ideone.com/fork/9rhnLz
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack{
+	int arr[10000],end;
+	public:
+	Stack(){
+		end=0;
+	}
+	void push(int val){
+		arr[end]=val;
+		cout<<"value added = "<<arr[end]<<endl;
+		end++;
+	}
+	int top(){
+		// cout<<arr[end-1]<<endl;
+		return arr[end-1];
+	}
+	int pop(){
+		cout<<arr[end-1]<<endl;
+		end--;
+		return 0;
+	}
+	int size(){
+		return end;
+	}
+};
+int main() {
+	Stack s;
+	s.push(22);
+	cout<<"top : "<<s.top()<<endl;
+	s.push(11);
+	s.push(69);
+	s.push(7);
+	cout<<"top : "<<s.top()<<endl;
+	cout<<"popped element : "<<s.pop()<<endl;
+	cout<<"top : "<<s.top()<<endl;
+	return 0;
+}
