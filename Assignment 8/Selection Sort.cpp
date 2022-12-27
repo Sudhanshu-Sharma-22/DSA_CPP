b1e5fa93153a3cec8c157b307d6896feb05279ee
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2022-12-28 00:20:12 +0530

    Create Selection Sort.cpp

---
+++ Assignment 8/Selection Sort.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void PrintSortedArray(vector<int>&ans){
+	for(int i=0;i<ans.size();i++) cout<<ans[i]<<" ";
+}
+
+int maxindex(vector<int>&arr,int i){
+	for(int index=i+1;index<arr.size();index++)	{
+			if(arr[index]<arr[i]) {
+				i=index;
+			}
+	}
+	return i;
+}
+
+void sort(vector<int>&arr){
+	for(int i=0;i<arr.size();i++){
+		int index=maxindex(arr,i);
+		swap(arr[i],arr[index]);
+	}
+}
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int>nums(n);
+	cout<<"Before Sorting : ";
+	for(int i=0;i<n;i++) {
+		cin>>nums[i];
+		cout<<nums[i]<<" ";
+	}
+	sort(nums);
+	cout<<endl<<"After Sorting : ";
+	PrintSortedArray(nums);
+	return 0;
+}
