c5cc1792b6490c060de72bb01e75c9d82d85e063
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2023-01-08 20:10:48 +0530

    Create Camel Case.cpp

---
+++ JAN 9 Test Prac/Camel Case.cpp
@@ -0,0 +1,37 @@
+
+//https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    int count=1;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>=65 && s[i]<=90) count++;
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
