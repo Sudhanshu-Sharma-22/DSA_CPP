410402a137ccd637c763371c0171a4d267651444
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2023-01-09 00:07:36 +0530

    Update Tower of Hanoi.cpp

--- Assignment 6.1/Tower of Hanoi.cpp
+++ Assignment 6.1/Tower of Hanoi.cpp
@@ -1,1 +1,25 @@
-//working
+
+//https://www.hackerrank.com/contests/launchpad-1-winter-challenge/challenges/shift-plates/submissions/code/1354648547
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void towerOfHanoi(int, char, char, char);
+
+int main()
+{
+    int totalDisc;
+    cin >> totalDisc;
+    towerOfHanoi(totalDisc, 'A', 'C', 'B');
+    return 0;
+}
+
+void towerOfHanoi(int totalDisc, char A, char B, char C)
+{
+    if(totalDisc > 0)
+    {
+        towerOfHanoi(totalDisc-1, A, C, B);
+        cout << "Moving ring " << totalDisc << " from " << A << " to " << C << endl;
+        towerOfHanoi(totalDisc-1, B, A, C);
+    }
+}
