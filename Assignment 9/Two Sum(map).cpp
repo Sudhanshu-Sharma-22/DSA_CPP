2aab27694aaca557a3339b9842717e925c027994
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2023-03-04 18:22:05 +0530

    Update Two Sum(map).cpp

--- Assignment 9/Two Sum(map).cpp
+++ Assignment 9/Two Sum(map).cpp
@@ -1,1 +1,14 @@
 
+//https://leetcode.com/problems/two-sum/submissions/908853862/?envType=study-plan&id=data-structure-i
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) == mp.end()) mp[nums[i]] = i;
+            else return {mp[target - nums[i]], i};
+        }
+        return {-1,-1};
+    }
+};
